2532186d937620321639543be48d48448a2ab04b
Sebastian Kreft <skreft@gmail.com>
2022-02-14 13:54:54 -0300

    feat: add support for reporting click events in specific areas
    We add the option to specify which areas are going to trigger a click event. By default if no sub element with a `data-ts-clickable` attribute is defined, then the whole container will be considered clickable.

--- src/detector.clicks-select-child.ts
+++ src/detector.clicks-select-child.ts
@@ -7,15 +7,30 @@ test("check clicks", async () => {
     events.push((e as any).detail);
   });
   document.body.innerHTML = `
-    <div id="product" data-ts-product="product-id-click-1" data-ts-auction="1247eaae-63a1-4c20-9b52-9efdcdef3095"></div>
+    <div id="product" data-ts-product="product-id-click-1" data-ts-auction="1247eaae-63a1-4c20-9b52-9efdcdef3095"><div id="clickable1" data-ts-clickable>Clickable 1</div><div id="non-clickable">Non clickable Text</div><div id="clickable2" data-ts-clickable>Clickable 2</div></div>
   `;
   await import("./detector");
 
+  // These do not generate a click event
   document.getElementById("product")?.click();
+  document.getElementById("non-clickable")?.click();
+  expect(events).toHaveLength(1);
+
+  document.getElementById("clickable1")?.click();
+  document.getElementById("clickable2")?.click();
+
   const uid = events[0]?.uid;
   expect(events).toMatchObject([
     {
       type: "impression",
+      page: "/",
+      product: "product-id-click-1",
+      auction: "1247eaae-63a1-4c20-9b52-9efdcdef3095",
+      id: expect.stringMatching(/[\d.a-zA-Z-]+/),
+      uid,
+    },
+    {
+      type: "click",
       page: "/",
       product: "product-id-click-1",
       auction: "1247eaae-63a1-4c20-9b52-9efdcdef3095",

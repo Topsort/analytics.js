d13a3b6abc085e386f54b57ab2ff24958393a820
Johan Bergström <johan@topsort.com>
2023-08-02 19:48:55 +0100

    fix: lint

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/detector.ts"),
       name: "ts",
-      fileName: (format) => `ts.${format === "es" ? "mjs" : format === "umd" ? "js" : "js"}`,
+      fileName: (format) =>
+        `ts.${format === "es" ? "mjs" : format === "umd" ? "js" : "js"}`,
       formats: ["es", "umd"],
     },
     emptyOutDir: false,

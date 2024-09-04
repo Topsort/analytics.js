a68e649da477dc93c0a18fb8548c9065525e53da
Sebastian Kreft <skreft@topsort.com>
2024-09-04 12:10:50 -0400

    fix formatting

--- src/set.ts
+++ src/set.ts
@@ -1,10 +1,10 @@
-export function truncateSet<T>(set: Set<T>, maxSize: number): Set<T>{
-    if (set.size <= maxSize) {
-        return set;
-    }
-    const iterator = set.values();
-    for (let i = 0; i < set.size - maxSize; ++i) {
-        iterator.next();
-    }
-    return new Set(iterator);
+export function truncateSet<T>(set: Set<T>, maxSize: number): Set<T> {
+  if (set.size <= maxSize) {
+    return set;
+  }
+  const iterator = set.values();
+  for (let i = 0; i < set.size - maxSize; ++i) {
+    iterator.next();
+  }
+  return new Set(iterator);
 }
